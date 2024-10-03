5c016ad49ffd8415a460ad24702689fcfeccc3d8
Muskan0423 <muskankewlani123@gmail.com>
2024-10-04 02:20:18 +0530

    changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,16 +3,23 @@ import { FiFacebook } from "react-icons/fi";
 import { FaInstagram } from "react-icons/fa6";
 import { CiTwitter } from "react-icons/ci";
 import { LuTwitch } from "react-icons/lu";
+import { CiYoutube } from "react-icons/ci";
 const Footer = () => {
   return (
-    <footer className="bg-black text-white py-6">
+    <footer className="bg-black text-white py-12">
       <div className="container mx-auto px-4">
-        <div className="flex justify-center space-x-4 mb-6">
+        <div className="flex justify-center space-x-9  mb-6">
           <a href="#" className="text-white"><FiFacebook /></a>
           <a href="#" className="text-white"><FaInstagram /></a>
           <a href="#" className="text-white"><CiTwitter/></a>
-          <a href="#" className="text-white"><i className="fab fa-twitch"></i></a>
-          <a href="#" className="text-white"><i className="fab fa-youtube"></i></a>
+          <a href="#" className="text-white"><LuTwitch /></a>
+          <a href="#" className="text-white"><CiYoutube /></a>
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 text-center text-sm">
+          <a href="#" className="text-white">Privacy Policy</a>
+          <a href="#" className="text-white">Contact Us</a>
+          <a href="#" className="text-white">Cookie Preferences</a>
+          <a href="#" className="text-white">Corporate Information</a>
         </div>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4 text-center text-sm">
           <a href="#" className="text-white">Privacy Policy</a>
--- src/Pages/SignUpFirst.jsx
+++ src/Pages/SignUpFirst.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const SignUpFirst = () => {
   return (
-    <div><p>SignUp</p>
-      
+    <div className='flex justify-center items-center bg-gray-100'>
+    <div className=" mt-14 mb-12 ">
+      <div className="bg-white p-20 rounded-lg shadow-lg w-full max-w-4xl flex">
+        <div className="w-full pr-8">
+          <img src="logo_url" alt="Logo" className="mb-4" /> 
+          <h2 className="text-lg font-semibold mb-2">STEP 1</h2>
+          <h1 className="text-3xl font-bold mb-4">Enter your email address to continue</h1>
+          <p className="text-gray-600">Log in to your account. If you don't have one, you will be prompted to create one.</p>
+        </div>
+        <div className="w-full flex flex-col justify-center">
+          <input
+            type="email"
+            placeholder="Email"
+            className="p-3 border rounded-lg w-full mb-4 focus:outline-none focus:border-black"
+          />
+          <button className="bg-black text-white px-6 py-3 rounded-lg">Continue</button>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
